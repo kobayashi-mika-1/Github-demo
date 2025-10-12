9d91fd531012d22e53c5c8143c9c14ad76956321
kobayashi-mika-1 <pokko.zenkoku@gmail.com>
2025-10-12 18:52:40 +0900

    [課題１１]Read処理のServiceとController部分を実装 ## 実装内容 ①年齢が３０代の人のみ抽出 ②「Java」コースの人のみ抽出
    ## 動作確認
    ### 1. 全件検索成功時
    [スクリーンショット]
    
    ## 学んだこと
    ・検索方法が二通りあること。
    ・今回の課題を取り組むことで、今までの復習を行うことができた。
    ・命名規則の確認

--- src/main/java/raisetech/StudentManagement/controllar/StudentControllar.java
+++ src/main/java/raisetech/StudentManagement/controllar/StudentControllar.java
@@ -4,24 +4,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.Students_Courses;
+import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
 @RestController
 public class StudentControllar {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentControllar(StudentService service) {
+  public StudentControllar(StudentService service , StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
-  public List<Student> getsearchStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getsearchStudentList() {
+    List<Student> students = service.searchStudentList();
+    List<Students_Courses> studentsCourses = service.searchStudents_CoursesList();
+
+    return converter.convertStudentDetails(students, studentsCourses);
   }
+
+
 
   @GetMapping("/studentCourseList")
   public List<Students_Courses> getStudents_CoursestList() {
---
+++ src/main/java/raisetech/StudentManagement/converter/StudentConverter.java
@@ -0,0 +1,30 @@
+package raisetech.StudentManagement.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.Students_Courses;
+import raisetech.StudentManagement.domain.StudentDetail;
+
+@Component
+public class StudentConverter {
+
+  public List<StudentDetail> convertStudentDetails(List<Student> students,
+      List<Students_Courses> studentsCourses) {
+    List<StudentDetail> studentDetailList = new ArrayList<>();
+    students.forEach(student -> {
+      StudentDetail studentDetail = new StudentDetail();
+      studentDetail.setStudent(student);
+      List<Students_Courses> convertStudentCourses = studentsCourses.stream()
+          .filter(studentsCourse -> student.getId() == studentsCourse.getStudentsId())
+          .collect(Collectors.toList());
+      studentDetail.setStudentsCourses(convertStudentCourses);
+      studentDetailList.add(studentDetail);
+    });
+    return studentDetailList;
+  }
+
+}
