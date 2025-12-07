7157cc6ed98e85162fe4172578255112097d278e
kobayashi-mika-1 <pokko.zenkoku@gmail.com>
2025-12-07 17:14:37 +0900

    [課題１４]Thymeleafを使ったReadの画面描画処理 ## 実装内容 ・localhost:8080/studentListにアクセスして受講生一覧を表示する。
    ## 動作確認
    ### 1. 受講生一覧の表示画面
    [スクリーンショット]
    
    ## 学んだこと
    ・画面で確認する方法

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -4,13 +4,13 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import raisetech.StudentManagement.data.Student;
-import raisetech.StudentManagement.data.Students_Courses;
+import raisetech.StudentManagement.data.students_Courses;
 
 @Getter
 @Setter
 public class StudentDetail {
 
   private Student student;
-  private List<Students_Courses> studentsCourses;
+  private List<students_Courses> studentsCourses;
 
 }
